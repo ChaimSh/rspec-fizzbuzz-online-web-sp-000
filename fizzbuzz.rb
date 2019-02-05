17991caaeac01fa83c701f88b4e3022a8baa8019
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-02-05 03:14:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  end
-  if int % 5 == 0
+  elsif
+     int % 5 == 0
     "Buzz"
-  end
-  if int % 3 && 5 == 0
+  elsif 
+    int % 3 && 5 == 0
     "FizzBuzz"
   end
 end
